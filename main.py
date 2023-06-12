aa952c000e55584acb1447b08b417eb9af80466e
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-06-12 16:44:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,16 @@
 # A Python Program to convert LaTeX to python code or vice versa.
-
+from process_latex import process_sympy
 
 def latex_to_py(latex, use_standard=True):
-    """Converts LaTeX to python code."""
-    latex = latex.replace("\\", "\\\\")
-    latex = latex.replace("{", "{{")
-    latex = latex.replace("}", "}}")
-    latex = latex.replace("(", "{")
-    latex = latex.replace(")", "}")
-    latex = latex.replace("^", "**")
-    latex = latex.replace("sqrt", "math.sqrt")
-    latex = latex.replace("frac", "Fraction")
-    latex = latex.replace("pi", "math.pi")
-    latex = latex.replace("sin", "math.sin")
-    latex = latex.replace("cos", "math.cos")
-    latex = latex.replace("tan", "math.tan")
-    latex = latex.replace("cot", "math.cot")
-    latex = latex.replace("sec", "math.sec")
-    latex = latex.replace("csc", "math.csc")
-    latex = latex.replace("log", "math.log")
-    latex = latex.replace("ln", "math.ln")
-    latex = latex.replace("e", "math.e")
-    latex = latex.replace("lim", "Limit")
-    latex = latex.replace("int", "Integral")
-    latex = latex.replace("sum", "Sum")
-    latex = latex.replace("prod", "Product")
-    latex = latex.replace("to", "to")
-    latex = latex.replace("infty", "oo")
-    latex = latex.replace("infinity", "oo")
-    latex = latex.replace("in", "in")
-    return latex
+    """Converts LaTeX to Python code."""
+    sympy = process_sympy(latex)
+    # TODO: Convert sympy to python code
+    return sympy
 
+def py_to_latex(py):
+    """Converts Python code to LaTeX."""
+    pass
 
 if __name__ == "__main__":
-    latex = 
+    latex = "\\delta \\ln{(R_{avg})}=\\sqrt{\\left(\\frac{\\partial \\ln{R_{avg}}}{\\partial R_{avg}}\\delta R_{avg} \\right)^2}"
     print(latex_to_py(latex))
